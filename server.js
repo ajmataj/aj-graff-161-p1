97313d8e4f0e33d6728256d5e7c56c4db3457afa
BuildTools <unconfigured@null.spigotmc.org>
2019-10-22 21:32:56 -0500

    Added working database connection

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import connectDatabase from './config/db';
-import insertData from './config/db';
 import { check, validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
+import Card from './models/Card';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 // Initialize express application
 const app = express();
@@ -32,23 +35,39 @@ app.post(
         check('name', 'Please enter your name').not().isEmpty(),
         check('cardNum', 'Please enter the 16 digit credit card number').isInt().isLength(16),
         check('expMonth', 'Please enter the expiration month (MM)').isInt().isLength({ min: 2, max: 2 }),
-        check('expYear', 'Please enter the expiration year (YY)').isInt().isLength({ min: 2, max: 2 })
+        check('expYear', 'Please enter the expiration year (YY)').isInt().isLength({ min: 2, max: 2 }),
+        check('cvv', 'Please enter the three-digit cvv').isInt().isLength({ min: 3, max: 3 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         } else {
-            // return res.send(req.body);
-            var card = {
-                'name': req.body.name,
-                'cardNum': req.body.cardNum,
-                'expMonth': req.body.cardNum,
-                'expYear': req.body.expYear,
-                'cvv': req.body.cvv
-            }
+            const { name, cardNum, expMonth, expYear, cvv } = req.body;
+            try {
+                // Check if card exists
+                let card = await Card.findOne({ cardNum: cardNum });
+                if (card) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Card number already exists' }] });
+                }
 
-            insertData(card);
+                // Create new card
+                card = new Card({
+                    name: name,
+                    cardNum: cardNum,
+                    expMonth: expMonth,
+                    expYear: expYear,
+                    cvv: cvv
+                });
+
+                // Save to the db and return
+                await card.save();
+                res.send('Card successfully registered');
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
         }
     }
 );
